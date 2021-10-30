6bb5eac574ce2fd14f027b68054d04fd38dad57f
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2021-10-30 15:46:47 -0300

    refactor: atualizando taskController. Colocando validação de email;

--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -5,6 +5,8 @@ const saveTask = async (req, res) => {
 
   const createdAt = new Date();
 
+  if (!email) return res.status(400).json({ message: "User not loged in or does not exists!" })
+
   const result = await service.saveTask(task, email, createdAt);
 
   res.status(200).json(result);

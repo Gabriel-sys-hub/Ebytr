6f24cf43352bfa6ada7b5839656c97670dd110cd
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2021-10-30 23:06:31 -0300

    refac: mudando rota para /:email, para recuperar o email via params;

--- server/router/tasksRouter.js
+++ server/router/tasksRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const taskController = require('../controller/taskController');
 
 router.post('/', taskController.saveTask);
-router.get('/', taskController.getAllTasks);
+router.get('/:email', taskController.getAllTasks);
 router.put('/', taskController.updateTask);
 
 

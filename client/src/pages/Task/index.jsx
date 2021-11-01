3974c1ba874a1515a1c577879d179bd6ca0cc310
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2021-10-31 21:25:39 -0300

    feat: delete task - styles: react icons

--- client/src/pages/Task/index.jsx
+++ client/src/pages/Task/index.jsx
@@ -1,9 +1,19 @@
 import React, { useContext, useState, useEffect } from 'react';
 import UserContext from '../../contexts/UserContext';
+import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
+import { BiTask } from 'react-icons/bi';
 import './styles.scss';
 
+
 function Task() {
+<<<<<<< Updated upstream
   const { login: { email }, handleAllTasks, handleSavedTasks, handleInputTask, tasks } = useContext(UserContext);
+=======
+<<<<<<< Updated upstream
+  const context = useContext(UserContext);
+=======
+  const { login: { email }, handleAllTasks, handleSavedTasks, handleInputTask, tasks, deleteTask } = useContext(UserContext);
+>>>>>>> Stashed changes
   const [logedUser, setLogedUser] = useState();
   
   useEffect(() => {
@@ -16,12 +26,18 @@ function Task() {
 
     if (email) handleAllTasks(email);
     if (!email) handleAllTasks(logedUser);
+<<<<<<< Updated upstream
   },[email, setLogedUser, logedUser])
+=======
+  },[email, setLogedUser, logedUser, deleteTask])
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 
   return (
   <div className="mainContainer">
     <header className="firstHeader">
+<<<<<<< Updated upstream
       <button>
         B1
       </button>
@@ -37,10 +53,34 @@ function Task() {
       {tasks.length > 0 && tasks.map((eachItem) => {
         return (
           <div className="eachTaskContainer" key={eachItem.id}>
+=======
+<<<<<<< Updated upstream
+      a
+    </header>
+    <main className="taskContainer">
+      a
+=======
+      <button>
+        <BiTask/>
+      </button>
+      <button>
+        B2
+      </button>
+      <button>
+        B3
+      </button>
+      
+    </header>
+    <main className="taskContainer">
+      {tasks.length > 0 && tasks.map((eachItem) => {
+        return (
+          <div className="eachTaskContainer" key={eachItem._id}>
+>>>>>>> Stashed changes
             <div className="task">
               {eachItem.task}
             </div>
             <div className="buttonContainer">
+<<<<<<< Updated upstream
               <button>
                 B1
               </button>
@@ -49,11 +89,22 @@ function Task() {
               </button>
               <button>
                 B1
+=======
+              <button type="button" value={eachItem._id} onClick={(event) => deleteTask(event)}>
+                <AiFillDelete/>
+              </button>
+              <button>
+                <AiFillEdit/>
+>>>>>>> Stashed changes
               </button>
             </div>
           </div>
         )
       })}
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     </main>
     <header className="secondHeader">
       <div className="picture">

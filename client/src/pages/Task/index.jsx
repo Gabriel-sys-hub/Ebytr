34bde0e27a3a4967e87061c8d23898d68e1d0de0
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2021-10-31 21:41:58 -0300

    conflict: resolvendo conflito

--- client/src/pages/Task/index.jsx
+++ client/src/pages/Task/index.jsx
@@ -6,14 +6,7 @@ import './styles.scss';
 
 
 function Task() {
-<<<<<<< Updated upstream
-  const { login: { email }, handleAllTasks, handleSavedTasks, handleInputTask, tasks } = useContext(UserContext);
-=======
-<<<<<<< Updated upstream
-  const context = useContext(UserContext);
-=======
   const { login: { email }, handleAllTasks, handleSavedTasks, handleInputTask, tasks, deleteTask } = useContext(UserContext);
->>>>>>> Stashed changes
   const [logedUser, setLogedUser] = useState();
   
   useEffect(() => {
@@ -26,40 +19,11 @@ function Task() {
 
     if (email) handleAllTasks(email);
     if (!email) handleAllTasks(logedUser);
-<<<<<<< Updated upstream
-  },[email, setLogedUser, logedUser])
-=======
-  },[email, setLogedUser, logedUser, deleteTask])
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-
+  }, [logedUser])
 
   return (
   <div className="mainContainer">
     <header className="firstHeader">
-<<<<<<< Updated upstream
-      <button>
-        B1
-      </button>
-      <button>
-        B2
-      </button>
-      <button>
-        B3
-      </button>
-      
-    </header>
-    <main className="taskContainer">
-      {tasks.length > 0 && tasks.map((eachItem) => {
-        return (
-          <div className="eachTaskContainer" key={eachItem.id}>
-=======
-<<<<<<< Updated upstream
-      a
-    </header>
-    <main className="taskContainer">
-      a
-=======
       <button>
         <BiTask/>
       </button>
@@ -75,36 +39,20 @@ function Task() {
       {tasks.length > 0 && tasks.map((eachItem) => {
         return (
           <div className="eachTaskContainer" key={eachItem._id}>
->>>>>>> Stashed changes
             <div className="task">
               {eachItem.task}
             </div>
             <div className="buttonContainer">
-<<<<<<< Updated upstream
-              <button>
-                B1
-              </button>
-              <button>
-                B1
-              </button>
-              <button>
-                B1
-=======
               <button type="button" value={eachItem._id} onClick={(event) => deleteTask(event)}>
                 <AiFillDelete/>
               </button>
               <button>
                 <AiFillEdit/>
->>>>>>> Stashed changes
               </button>
             </div>
           </div>
         )
       })}
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     </main>
     <header className="secondHeader">
       <div className="picture">
